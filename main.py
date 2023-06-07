a797ce017f76d741e3a87463b2d12310b3e2dd8c
LeoJPG <lyl102938475647382910@gmail.com>
2023-06-07 18:59:56 +0200

    Minor refactor and bugfix

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import os
 import platform
-import traceback
 import shutil
-from supported_systems import SupportedSystemsPaths
+import traceback
+import time
+
 from configuration_store import Config
+from supported_systems import SupportedSystemsPaths
 
-
-"""
-Detects the type of system (Windows or Linux) and changes directory to the downloads directory accordingly.
-"""
-def go2sortingdir(os_name, config: Config) -> str:
-    
+def go2sortingdir(config: Config) -> str:
+    """
+    Detects the type of system (Windows or Linux) and changes directory to the downloads directory accordingly.
+    """
     #print(os_name)
-    path = generate_full_destination_path("%s%s%s" % (get_home_directory(), config.file_path_seperator, config.sorting_directory), config)
+    path = generate_full_destination_path("%s%s%s" % (get_home_directory(config.system_name), config.file_path_seperator, config.sorting_directory), config)
     #print(supported_system_enums)
     #print(len(dir(SupportedSystemsPaths)))
 
-    
+
     #print(path)
     #print(os.getcwd())
     os.chdir(path)
@@ -26,34 +26,37 @@ def go2sortingdir(os_name, config: Config) -> str:
 def get_file_list() -> list:
     return [file for file in os.listdir() if os.path.isfile(file)]
 
-"""
-Moves all files in the sorting directory to the destination directory.
-"""
+
 def move_files(config: Config):
+    """
+    Moves all files in the sorting directory to the destination directory.
+    """
     file_list = get_file_list()
     for file_name in file_list:
-        file_extension = file_name.split('.')[1]
+        file_extension = file_name.split('.')[-1]
         if file_extension in config.file_destination_paths.keys():
             destination = "%s%s%s" % (config.file_destination_paths[file_extension], config.file_path_seperator, file_name)
             print(destination)
             shutil.move(file_name, generate_full_destination_path(destination, config))
 
 
-"""
-Takes the path_string and checks if it's a full path then it just returns it and if it it's not then 
-returns the relative path from home directory to path_string.
-"""
+
 def generate_full_destination_path(path_string, config: Config):
+    """
+    Takes the path_string and checks if it's a full path then it just returns it and if it it's not then
+    returns the relative path from home directory to path_string.
+    """
     if path_string[0] in ("\\", "/", "~"):
         return path_string
     else:
-        return "%s%s%s" % (get_home_directory(), config.file_path_seperator, path_string)
+        return "%s%s%s" % (get_home_directory(config.system_name), config.file_path_seperator, path_string)
 
 
-"""
-Quite self-explanatory, it gets the home directory.
-"""
-def get_home_directory() -> str:
+
+def get_home_directory(os_name: str) -> str:
+    """
+    Quite self-explanatory, it gets the home directory.
+    """
     path = ""
     supported_system_enums = dir(SupportedSystemsPaths)[:len(dir(SupportedSystemsPaths))-11]
     if os_name.upper() in supported_system_enums:
@@ -64,13 +67,18 @@ def get_home_directory() -> str:
     return path.value
 
 
-if __name__ == '__main__':
-    #Gets the system name like Windows, Linux, etc.
+def run():
+    # Gets the system name like Windows, Linux, etc.
     os_name = platform.system()
-    #Loads the configuration of the destination directory for the file types.
+    # Loads the configuration of the destination directory for the file types.
     config = Config(os_name)
-    #Changes directory to the Dowload directory.
-    path = go2sortingdir(os_name, config)
-    #print(get_file_list())
-    #print(config.file_destination_paths)
-    move_files(config)
+    while True:
+        # Changes directory to the Dowload directory.
+        path = go2sortingdir(config)
+        # print(get_file_list())
+        # print(config.file_destination_paths)
+        move_files(config)
+        time.sleep(5)
+
+if __name__ == '__main__':
+    run() # Runs :3
